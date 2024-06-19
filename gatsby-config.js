6bf906c33d8933288f7cdebd7fb643caad11a018
Rubul Hoque Choudhury <rubulhoquechoudhury123@gmail.com>
2024-06-19 15:17:58 +0530

    1st commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+  pathPrefix: "/hello",
+}
---
+++ src/pages/hello.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import {Link}  from "gatsby-Link";
+
+export default function Home() {
+    return <div>Hello world rubul
+      <h1>gweg</h1>
+      <img src="https://avatars.githubusercontent.com/u/113061529?v=4" alt="RUBUL" />
+      
+      <Link to="/">HOME</Link>
+    </div>
+  }
+  export const Head = () => <title>HELLO</title>
