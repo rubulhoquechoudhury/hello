9cf05b6a567b0ef5e2b64e6257e1877bb81aaaca
Rubul Hoque Choudhury <rubulhoquechoudhury123@gmail.com>
2024-06-19 15:58:49 +0530

    4th commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,11 +131,10 @@ const IndexPage = () => {
       <h1 style={headingStyles}>
         Congratulations
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
+        
       </h1>
       <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
+        Hello, Welcome <code style={codeStyles}>Classes 365</code> new age of learning
       </p>
       <ul style={listStyles}>
         <li style={docLinkStyle}>
