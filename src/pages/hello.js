7e6e32afbebcc513500c4b69c7291d575ce696f6
Rubul Hoque Choudhury <113061529+rubulhoquechoudhury@users.noreply.github.com>
2024-06-23 18:43:00 +0530

    Update hello.js

--- src/pages/hello.js
+++ src/pages/hello.js
@@ -4,7 +4,7 @@ import {Link}  from "gatsby";
 export default function Home() {
     return <div>Hello world rubul
       <h1>gweg</h1>
-      <img src="https://avatars.githubusercontent.com/u/113061529?v=4" alt="RUBUL" />
+      
       <h2>pjivcjiev vv jiej</h2>
       
       <Link to="/">HOME</Link>
